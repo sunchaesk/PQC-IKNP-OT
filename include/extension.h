c68c900c3d113d78ae7e3843480c6d3ad92ac1d3
Chris Kim <sunchaesk@gmail.com>
2025-11-11 22:52:16 -0500

    random matrix gen

--- include/extension.h
+++ include/extension.h
@@ -8,19 +8,30 @@
 #include <vector>
 #include <memory>
 
+#include <assert.h>
+
+
 class OTExtensionReceiver {
 private:
     uint32_t security_parameter;
     uint32_t num_ots;
 
     std::vector<std::unique_ptr<BaseOTReceiver>> base_ot_receivers;
+    std::vector<uint8_t> choice_bits;
 
 public:
-    OTExtensionReceiver(uint32_t security_parameter, uint32_t num_ots)
+    OTExtensionReceiver(uint32_t security_parameter, 
+                        uint32_t num_ots,
+                        const std::vector<uint8_t>& choice_bits)
      : security_parameter(security_parameter),
-       num_ots(num_ots) {
+       num_ots(num_ots),
+       choice_bits(choice_bits) {
 
         initialize_base_ots();
+        generate_random_matrices_t_u();
+
+        assert(choice_bits.size() == num_ots);
+
     }
 
     uint32_t get_security_parameter() const {
@@ -28,6 +39,7 @@ public:
     }
 
     void initialize_base_ots();
+    void generate_random_matrices_t_u();
 };
 
 class OTExtensionSender {
--- src/extension.cpp
+++ src/extension.cpp
@@ -11,11 +11,34 @@ void OTExtensionReceiver::initialize_base_ots() {
     for (size_t i = 0; i < this->security_parameter; ++i) {
         base_ot_receivers.push_back(std::make_unique<BaseOTReceiver>());
     }
-
-    
-
 }
 
+
+void OTExtensionReceiver::generate_random_matrices_t_u() {
+    uint32_t k = this->security_parameter;
+    uint32_t m = this->num_ots;
+
+    // m rows, k cols
+    std::vector<std::vector<uint8_t>> t_maxtrix, u_matrix;
+    t_maxtrix.resize(m, std::vector<uint8_t>(k, 0));
+    u_matrix.resize(m, std::vector<uint8_t>(k, 0));
+
+    // iterate over the rows --> at i-th row
+    for (size_t i = 0; i < m; ++i) {
+        for (size_t j = 0; j < k; ++j) {
+            t_maxtrix[i][j] = rand() % 2; // random bit
+        }
+    }
+
+    // construct u matrix
+    // for row i -> u_i = t_i XOR choice_bit_i
+    for (size_t i = 0; i < m; ++i) {
+        uint8_t c = choice_bits[i];
+        for (size_t j = 0; j < k; ++j) {
+            u_matrix[i][j] = t_maxtrix[i][j] ^ c;
+        }
+    }
+}
 ///////////////////////////////////
 // OT Extension Sender
 ///////////////////////////////////
